7c51a832fc253d7ba3e6c9b661cdf33bc508b171
faisal-kn <khanmohdfaisal75@gmail.com>
2021-08-20 10:39:30 +0530

    Database Connected

---
+++ server.js
@@ -0,0 +1,22 @@
+const dotenv = require("dotenv");
+const app = require("./app");
+const mongoose = require("mongoose");
+
+dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DATABASE;
+
+mongoose
+  .connect(DB, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("Database successfully connected"));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "127.0.0.1", () => {
+  console.log("listening to request on port 3000");
+});
