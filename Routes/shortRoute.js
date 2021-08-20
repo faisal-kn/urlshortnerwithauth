e0fa1281b3841bb1ee7babe57c762fa85a066bf7
faisal-kn <khanmohdfaisal75@gmail.com>
2021-08-20 11:32:31 +0530

    Added routes

---
+++ Routes/shortRoute.js
@@ -0,0 +1,5 @@
+const express = require("express");
+
+const shortRouter = express.Router();
+
+module.exports = shortRouter;
--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const shortRouter = require("./Routes/shortRoute");
+const userRouter = require("./Routes/userRoute");
 
 const app = express();
+
+app.use("/api/v2/users", userRouter);
+app.use("/api/v2/shorturl", shortRouter);
 
 module.exports = app;
