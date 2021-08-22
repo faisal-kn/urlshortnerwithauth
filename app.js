c6f13480b0a19fbbcef7c03ee6e1b3bc4543827c
khanmn <khanmohdfaisal75@gmail.com>
2021-08-22 16:06:49 +0530

    Enabled proxy

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const cookieparser = require("cookie-parser");
+const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const shortRouter = require("./Routes/shortRoute");
 const userRouter = require("./Routes/userRoute");
 const cookieParser = require("cookie-parser");
 
 const app = express();
+app.enable('trust-proxy');
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
