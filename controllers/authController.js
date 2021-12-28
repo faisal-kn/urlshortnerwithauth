869a449206b7f4e4f838c627c9c3af0ea209b791
khanmn <khanmohdfaisal75@gmail.com>
2021-12-28 10:51:48 +0530

    checking

--- controllers/authController.js
+++ controllers/authController.js
@@ -62,9 +62,9 @@ exports.login = async (req, res, next) => {
 
     const cookieOptions = {
       expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
-      // httpOnly: true,
+      httpOnly: true,
       secure: true,
-      sameSite: "None",
+      // sameSite: "None",
     };
     res.cookie("auth", token, cookieOptions);
 
